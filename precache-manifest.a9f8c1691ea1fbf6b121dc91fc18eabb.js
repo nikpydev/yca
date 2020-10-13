075132a267b343bf8ad177b112972dafb047318e
Nikhil Choudhary <nc1336282@gmail.com>
2020-10-13 12:36:00 +0530

    Updates

---
+++ precache-manifest.a9f8c1691ea1fbf6b121dc91fc18eabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956566bb324645ef8ddfb9f3aa6caba4",
+    "url": "/yca/index.html"
+  },
+  {
+    "revision": "1de7e10a46036dd45b97",
+    "url": "/yca/static/css/main.75ca054a.chunk.css"
+  },
+  {
+    "revision": "86beeb684fdceab41a2b",
+    "url": "/yca/static/js/2.13c87c9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yca/static/js/2.13c87c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de7e10a46036dd45b97",
+    "url": "/yca/static/js/main.a7f4dd6a.chunk.js"
+  },
+  {
+    "revision": "c49c3855f03c2a2a14dd",
+    "url": "/yca/static/js/runtime-main.fe824f8c.js"
+  }
+]);
